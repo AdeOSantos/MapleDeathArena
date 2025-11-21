ce8ca16daab9d39e90eff8dc88b4f585b40f56c0
Adalberto Santos <adalbertoliveira@gmail.com>
2025-11-21 15:02:04 -0300

    Actuator and profiles working. Upgraded to Java 21. Dev profile with everything exposed and Prod profile with only Health and Info

--- src/main/java/com/mapledeatharena/MapleDeathArenaApplication.java
+++ src/main/java/com/mapledeatharena/MapleDeathArenaApplication.java
@@ -1,4 +1,14 @@
 package com.mapledeatharena;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class MapleDeathArenaApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(MapleDeathArenaApplication.class, args);
+        }
+
 }
